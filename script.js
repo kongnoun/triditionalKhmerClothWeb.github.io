855238df2cce9f84d1948581806c940fe5e0ef18
kongnoun <kongnoun00@gmail.com>
2024-05-20 18:13:04 +0700

    changes1

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// const hamburgerMenu = document.querySelector(".hamburger-menu");
-// const nav = document.querySelector(".nav");
+document.addEventListener('DOMContentLoaded', function() {
+    var navlinks = document.querySelector('navlinks');
+    var navbar= document.querySelector('navbar');
+    
+    if (navlinks.display === "block") {
+        navlinks.style.display = "none";
+    } else {
+        navlinks.style.display = "block";
+    }
 
-// hamburgerMenu.addEventListener("click", () => {
-//     nav.classList.toggle("active")
-// });
+    if (navbar.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "none";
+    }
+});
